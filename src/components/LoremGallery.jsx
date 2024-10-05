897292fcd9851b22b891dc11f66d067b2999b42b
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-05 11:10:48 +0100

    style: edit

--- src/components/LoremGallery.jsx
+++ src/components/LoremGallery.jsx
@@ -13,6 +13,7 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
+  Fade,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon, CloseIcon } from "@chakra-ui/icons";
@@ -64,6 +65,7 @@ export default function LoremGallery({ isOpen, onClose }) {
                   _hover={{ bg: "primary.2" }}
                   _active={{ bg: "primary.4" }}
                   onClick={handleLeft}
+                  aria-label="Previous image"
                 />
                 <IconButton
                   icon={<ChevronRightIcon color={"black"} boxSize={5} />}
@@ -72,6 +74,7 @@ export default function LoremGallery({ isOpen, onClose }) {
                   _hover={{ bg: "primary.2" }}
                   _active={{ bg: "primary.4" }}
                   onClick={handleRight}
+                  aria-label="Next image"
                 />
                 <IconButton
                   icon={<CloseIcon color={"black"} boxSize={"10px"} />}
@@ -80,22 +83,35 @@ export default function LoremGallery({ isOpen, onClose }) {
                   _hover={{ bg: "primary.2" }}
                   _active={{ bg: "primary.4" }}
                   onClick={onClose}
+                  aria-label="Close modal"
                 />
               </HStack>
             </Flex>
           </CardHeader>
           <Divider opacity={1} borderColor={"primary.5"} />
           <CardBody>
-            <Image
-              alt="Lorem"
-              src={`/lorem-${currentIndex}.png`}
-              width={1440}
-              height={810}
-              placeholder="blur"
-              blurDataURL={blurDataUrl}
-              loading="lazy"
-              style={{ borderRadius: "3px" }}
-            />
+            {Array.from({ length: imageCount }).map((_, index) => (
+              <>
+                {index === currentIndex && (
+                  <Fade
+                    in={true}
+                    offsetY={16}
+                    transition={{ enter: { delay: 0.09 } }}
+                  >
+                    <Image
+                      alt="Lorem"
+                      src={`/lorem-${index}.png`}
+                      width={1440}
+                      height={810}
+                      placeholder="blur"
+                      blurDataURL={blurDataUrl}
+                      loading="lazy"
+                      style={{ borderRadius: "3px" }}
+                    />
+                  </Fade>
+                )}
+              </>
+            ))}
           </CardBody>
         </Card>
       </ModalContent>
