e9c50a6b1c8691d0abd30a41608e47d8a34c5c5a
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-18 23:20:06 +0100

    remove redundant code

--- src/components/common/ButtonLink.js
+++ src/components/common/ButtonLink.js
@@ -1,35 +1,17 @@
 "use client";
 
-import { Button, Link } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
-export default function ButtonLink({ variant, icon, children, href, ...rest }) {
-  if (variant === "external") {
-    return (
-      <Button
-        variant={"link"}
-        as={Link}
-        target="_blank"
-        _active={{ color: "primary.6" }}
-        href={href}
-        leftIcon={icon}
-        fontSize={"xl"}
-        color={"primary.5"}
-        fontWeight={"normal"}
-      >
-        {children}
-      </Button>
-    );
-  }
-
+export default function ButtonLink({ icon, children, href, ...rest }) {
   return (
     <Button
       variant={"outline"}
       size={"lg"}
       as={NextLink}
       href={href}
-      rightIcon={<ArrowForwardIcon mt={"3px"} />}
+      rightIcon={<ArrowForwardIcon mt={"3px"} aria-hidden={"true"} />}
       fontSize={"xl"}
       {...rest}
     >
