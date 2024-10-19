1ff51c9a9565693fb22f883b53b5bb388cfaa162
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-19 04:29:48 +0100

    fix: styles

--- src/components/common/ButtonLink.js
+++ src/components/common/ButtonLink.js
@@ -1,10 +1,28 @@
 "use client";
 
-import { Button } from "@chakra-ui/react";
+import { Button, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
-export default function ButtonLink({ icon, children, href, ...rest }) {
+export default function ButtonLink({ variant, icon, children, href, ...rest }) {
+  if (variant === "external") {
+    return (
+      <Button
+        variant={"link"}
+        as={Link}
+        target="_blank"
+        _active={{ color: "primary.6" }}
+        href={href}
+        leftIcon={icon}
+        fontSize={"xl"}
+        color={"primary.5"}
+        fontWeight={"normal"}
+      >
+        {children}
+      </Button>
+    );
+  }
+
   return (
     <Button
       variant={"outline"}
