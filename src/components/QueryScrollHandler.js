1c229bd941894ca1a8e365c6bb489f1dd786f9bf
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-20 08:23:30 +0100

    combine code into one file

--- src/components/QueryScrollHandler.js
+++ src/components/QueryScrollHandler.js
@@ -1,9 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import useQueryScroll from "@/hooks/useQueryScroll";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function QueryScrollHandler() {
-  useQueryScroll();
+  const searchParams = useSearchParams();
+  const projectId = searchParams.get("id");
+
+  function scrollTo(id, offset = 68) {
+    window.scrollTo({
+      top:
+        document.querySelector(id).getBoundingClientRect().top -
+        offset +
+        window.scrollY,
+      behavior: "smooth",
+    });
+  }
+
+  useEffect(() => {
+    switch (projectId) {
+      case "lorem":
+        scrollTo("#pre-lorem-ref");
+        break;
+      case "meshara":
+        scrollTo("#pre-meshara-ref", -9);
+    }
+  }, []);
 
   return <></>;
 }
