76355281bb3610cdc06639d80f374fdd3bad9fb0
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-30 23:34:55 +0000

    content: edit

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,11 +21,12 @@ export default function About() {
           About
         </Heading>
         <Text>
-          Born and raised in London, I&apos;ve become a passionate 21 year old
-          developer with a strong foundation in both front-end and back-end
-          technologies. With expertise in JavaScript, React and Node.js amongst
-          many others, I thrive on building dynamic, user-friendly web
-          applications that deliver flawless experiences.
+          Born and raised in London, I&apos;ve become an experienced,
+          self-taught developer with a strong foundation in both front-end and
+          back-end technologies. With expertise in JavaScript, React and Node.js
+          amongst many others, I thrive on building dynamic, user-friendly web
+          applications that deliver flawless experiences—and can quickly pick up
+          any necessary technologies depending on the needs of the project.
         </Text>
         <Text>
           My approach combines a strong dedication for continuous learning with
