1662474a8fee24b6c1a63915e6d082c3d4ec669a
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-11-01 14:38:27 +0000

    content: update

--- src/app/projects/_components/TBPGallery.js
+++ src/app/projects/_components/TBPGallery.js
@@ -10,7 +10,7 @@ import mesharaBlurUrl from "@/data/mesharaBlurUrl";
 export default function TBPGallery() {
   return (
     <GalleryCard>
-      <GalleryCardHeader heading="Design Preview" />
+      <GalleryCardHeader heading="Frontend Preview" />
       <GalleryCardBody>
         <Fade in={true} transition={{ enter: { delay: 0.09 } }}>
           <GalleryImg
