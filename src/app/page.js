bb20d98d67a842806c69ce84972f54527c22e300
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-11-01 14:58:42 +0000

    style: edit

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <PageTransition>
       <SimpleSection avatar={{ src: "/avatars/optimised/main-op.svg" }}>
-        <Heading mb={{ base: 0, lg: 2 }}>{"Hi, I'm Nicoll"}</Heading>
+        <Heading>{"Hi, I'm Nicoll"}</Heading>
         <Text>
           {
             "I'm a full-stack web developer based in London driven by my philosophy of “building as I learn, learning as I build, succeeding as I grow”. I develop efficient and robust applications for the web and have an eagerness to keep expanding my skills, not only as a developer, but as a creator and collaborator."
