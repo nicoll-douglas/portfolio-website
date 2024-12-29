5b7b6b705fa21f236b1ee144db70777d9ac6721d
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2024-12-29 17:53:34 +0000

    update content

--- src/app/projects/_components/ExtraProject.js
+++ src/app/projects/_components/ExtraProject.js
@@ -4,18 +4,11 @@ import ExternalLink from "@/components/common/ExternalLink";
 export default function ExtraProject() {
   return (
     <Box maxW={"full"} as="section" aria-labelledby="extra">
-      <Heading size={"lg"} mb={1} id="extra">
+      <Heading size={"lg"} mb={4} id="extra">
         Extra
       </Heading>
-      <Text mb={6}>
-        As a programmer, every once in a while I like to take a break from
-        building software and indulge my passion and curiosity for the raw craft
-        as well as the innate problem solving skills it requires. A result of
-        this is my solution for a technical and intriguing coding problem I
-        challenged myself with—perhaps you may find it as satisyfing as I did:
-      </Text>
-      <ExternalLink href="https://nicoll-douglas.github.io/knights-shortest-path/">
-        Knight&apos;s Shortest Path | A Solution by Nicoll Douglas
+      <ExternalLink href="https://github.com/nicoll-douglas/advent-of-code-2024">
+        Advent of Code Solutions 2024
       </ExternalLink>
     </Box>
   );
