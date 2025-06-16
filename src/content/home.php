fd711ed7dfe832eb118fed7688841f427a8e3bab
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-06-16 01:50:52 +0100

    update content

--- src/content/home.php
+++ src/content/home.php
@@ -1,7 +1,7 @@
 <div class="simple-split">
   <div>
     <h1 class="larger">Hi, I'm Nicoll</h1>
-    <p>I specialise in web development and DevOps where I like to engineer robust applications and streamline development workflows. I take great pleasure in the interconnectedness of technology and creating scalable solutions that deliver as good as they perform.</p>
+    <p>I'm a web development and DevOps specialist who likes to engineer robust applications and streamline development workflows. I take great pleasure in the interconnectedness of technology and creating scalable solutions that deliver as good as they perform.</p>
     <a href="/projects" class="cta-link">
       View my projects
       <?php require alias("@icon/arrow.php") ?>
